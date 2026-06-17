e252a3300f7b600a6f69997b555831bf0ae7a000
xabierscs <xabierscs@ext.inditex.com>
2026-06-17 17:45:40 +0200

    add path variable to pagina1 endpoint in PathVariableController

--- informaticonfig/springboot-app1/src/main/java/com/informaticonfig/spring/app1/springboot_app1/controllers/PathVariableController.java
+++ informaticonfig/springboot-app1/src/main/java/com/informaticonfig/spring/app1/springboot_app1/controllers/PathVariableController.java
@@ -2,6 +2,7 @@ package com.informaticonfig.spring.app1.springboot_app1.controllers;
 
 import com.informaticonfig.spring.app1.springboot_app1.models.dto.ParametroDTO;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/variable")
 public class PathVariableController {
 
-    @GetMapping("/pagina1")
-    public ParametroDTO pagina1() {
+    @GetMapping("/pagina1/{mensaje}")
+    public ParametroDTO pagina1(@PathVariable String mensaje) {
         ParametroDTO parametro1 = new ParametroDTO();
+        parametro1.setInformacion(mensaje);
         return parametro1;
     }
 
